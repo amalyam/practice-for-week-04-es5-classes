e49e8078d19585248efdd53d99cd76008d22bc68
Amalya Megerman <amalyamegerman@gmail.com>
2022-12-06 20:51:54 -0500

    passes all tests

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,12 @@
-// Your code here
+Array.prototype.isEqual = function (array) {
+  if (this.length !== array.length) {
+    return false;
+  } else {
+    for (i = 0; i < array.length; i++) {
+      if (this[i] !== array[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
